bbb0fc8056f575fafb6200235412d6b9a33cdf27
sairevanth <sairevanth999@gmail.com>
2023-04-02 21:32:20 +0530

    update

--- prod-server/main.tf
+++ prod-server/main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "prod-server" {
  }
  
  provisioner "local-exec" {
- command = "ansible-playbook prod-bank-playbook.yml "
+ command = "ansible-playbook /var/lib/jenkins/workspace/project_k/prod-server/prod-bank-playbook.yml "
   } 
 }
 
